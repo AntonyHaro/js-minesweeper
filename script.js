652968d1d458613475f0f1e15ec6b0038c282916
AntonyHaro <antonyhpro@outlook.com>
2024-08-15 18:21:34 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,103 @@
+const createEmptyMatrix = (rows, cols) => {
+    let matrix = [];
+    for (let i = 0; i < rows; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < cols; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+    return matrix;
+};
+
+function addBombs(matrix, bombs) {
+    for (let i = 0; i < bombs; i++) {
+        let row = Math.floor(Math.random() * matrix.length);
+        let col = Math.floor(Math.random() * matrix.length);
+
+        while (matrix[row][col] == "x") {
+            row = Math.floor(Math.random() * matrix.length);
+            col = Math.floor(Math.random() * matrix.length);
+        }
+        matrix[row][col] = "x";
+        const nextCells = getNextCells(matrix, row, col);
+        console.log(nextCells);
+    }
+}
+
+function getNextCells(matrix, row, col) {
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+    const nextCells = [];
+
+    const directions = [
+        [-1, -1], // cima-esquerda
+        [-1, 0], // cima
+        [-1, 1], // cima-direita
+        [0, -1], // esquerda
+        [0, 1], // direita
+        [1, -1], // baixo-esquerda
+        [1, 0], // baixo
+        [1, 1], // baixo-direita
+    ];
+
+    for (const [dl, dc] of directions) {
+        const newRow = row + dl;
+        const newCol = col + dc;
+
+        if (
+            newRow >= 0 &&
+            newRow < rows &&
+            newCol >= 0 &&
+            newCol < cols &&
+            matrix[newRow][newCol] != "x"
+        ) {
+            const nextCell = matrix[newRow][newCol];
+
+            matrix[newRow][newCol] = nextCell + 1;
+            nextCells.push(matrix[newRow][newCol]);
+        }
+    }
+
+    nextCells.forEach((cell) => {
+        const newCell = cell + 1;
+        cell = newCell;
+    });
+
+    return nextCells;
+}
+
+function renderGame(matrix) {
+    const createElement = (tag, className, text) => {
+        const element = document.createElement(tag);
+        className ? (element.className = className) : undefined;
+        element.textContent = text;
+        return element;
+    };
+
+    matrix.forEach((row, row_index) => {
+        row.forEach((col, col_index) => {
+            const cell =
+                matrix[row_index][col_index] === "x"
+                    ? createElement(
+                          "div",
+                          "cell bomb",
+                          matrix[row_index][col_index]
+                      )
+                    : createElement(
+                          "div",
+                          "cell",
+                          matrix[row_index][col_index]
+                      );
+
+            renderSpace.appendChild(cell);
+        });
+    });
+}
+
+const renderSpace = document.getElementById("render-space");
+const matrix = createEmptyMatrix(9, 9);
+addBombs(matrix, 15);
+
+console.log("Matriz gerada:", matrix);
+
+renderGame(matrix);
