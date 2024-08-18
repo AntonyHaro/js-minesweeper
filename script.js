a3146ee8f4da1064889e002ace77536aff3e92e9
AntonyHaro <antonyhpro@outlook.com>
2024-08-18 11:59:31 -0300

    refatoração das funções de renderização e cliques

--- script.js
+++ script.js
@@ -72,49 +72,69 @@ function renderGame(matrix, renderSpace) {
         row.forEach((col, colIndex) => {
             const cell =
                 matrix[rowIndex][colIndex] === "x"
-                    ? createElement("div", "cell bomb cover", `${rowIndex}_${colIndex}`)
-                    : createElement("div", "cell cover", `${rowIndex}_${colIndex}`);
+                    ? createElement(
+                          "div",
+                          "cell bomb cover",
+                          `${rowIndex}_${colIndex}`
+                      )
+                    : createElement(
+                          "div",
+                          "cell cover",
+                          `${rowIndex}_${colIndex}`
+                      );
 
             cell.addEventListener("click", () =>
-                handleClick(matrix, rowIndex, colIndex, matrix[rowIndex][colIndex] || "")
+                handleClick(matrix, rowIndex, colIndex)
             );
             renderSpace.appendChild(cell);
         });
     });
 }
 
-function handleClick(matrix, row, col, cellValue) {
-    console.log(row, col);
-    const cell = document.getElementById( `${row}_${col}`);
+function handleClick(matrix, row, col) {
+    const cell = document.getElementById(`${row}_${col}`);
     if (!cell.classList.contains("cover") || fimJogo) return;
-    cell.innerHTML = cellValue;
-    cell.classList.remove("cover");
 
-    if (cellValue === "") {
-        const directions = [
-            [-1, -1],
-            [-1, 0],
-            [-1, 1],
-            [0, -1],
-            [0, 1],
-            [1, -1],
-            [1, 0],
-            [1, 1],
-        ];
-    
-        for (const [dl, dc] of directions) {
-            const newRow = row + dl;
-            const newCol = col + dc;
-            if (newRow < 0 || newRow > (matrix.length - 1) || newCol < 0 || newCol > (matrix[0].length - 1)) {
-                continue;
-            }
-            handleClick(matrix, newRow, newCol, matrix[newRow][newCol] || "");
-        }
+    cell.classList.remove("cover");
+    const cellValue = matrix[row][col];
+    cell.innerHTML = cellValue === "x" ? "💣" : cellValue || "";
+
+    if (cellValue === 0) {
+        revealNextCells(matrix, row, col);
     }
 
     if (cellValue === "x") {
         fimJogo = true;
-        revealBombs(matrix);
+        setTimeout(() => {
+            revealBombs(matrix);
+        }, 800);
+    }
+}
+
+function revealNextCells(matrix, row, col) {
+    const directions = [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+    ];
+
+    for (const [dl, dc] of directions) {
+        const newRow = row + dl;
+        const newCol = col + dc;
+        if (
+            newRow < 0 ||
+            newRow > matrix.length - 1 ||
+            newCol < 0 ||
+            newCol > matrix[0].length - 1
+        ) {
+            continue;
+        }
+        handleClick(matrix, newRow, newCol);
     }
 }
 
@@ -122,21 +142,14 @@ function revealBombs(matrix) {
     matrix.forEach((row, rowIndex) => {
         row.forEach((col, colIndex) => {
             if (matrix[rowIndex][colIndex] === "x") {
-                const cell =
-                    renderSpace.children[
-                        rowIndex * matrix[0].length + colIndex
-                    ];
-                cell.innerHTML = "💣";
-                revealBomb(cell);
+                const cell = document.getElementById(`${rowIndex}_${colIndex}`);
+                if (cell) {
+                    cell.innerHTML = "💣";
+                    cell.classList.remove("cover");
+                }
             }
         });
     });
-}
-
-function revealBomb(bomb) {
-    setTimeout(() => {
-        bomb.classList.remove("cover");
-    }, 800);
 }
 
 let fimJogo = false;
@@ -145,5 +158,7 @@ const renderSpace = document.getElementById("render-space");
 
 const matrix = createEmptyMatrix(9, 9);
 addBombs(matrix, 15);
+
+console.log(matrix);
 
 renderGame(matrix, renderSpace);
