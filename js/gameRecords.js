fa3f3ce3258e23d3768913d79c1108c1be7f87bc
AntonyHaro <antonyhpro@outlook.com>
2024-10-26 16:00:47 -0300

    adição da funcionalidade de filtros de registros

--- js/gameRecords.js
+++ js/gameRecords.js
@@ -8,16 +8,23 @@ function renderRecords() {
         return element;
     };
 
-    const gameRecords = JSON.parse(localStorage.getItem("gameRecords")) || [];
+    wins = 0;
+    defeats = 0;
+    recordsContainer.innerHTML = "";
 
     if (gameRecords.length <= 0) {
         noRecords.textContent = "No records were found, try playing a game!";
         return;
     }
 
-    recordsContainer.innerHTML = "";
+    noRecords.textContent = "";
     gameRecords.forEach((record) => {
         const recordElement = createElement("div", "record");
+        recordElement.dataset.result = record.result
+            ? "wins-filter"
+            : "defeats-filter";
+        recordElement.dataset.difficulty = record.difficulty;
+
         const result = createElement(
             "h2",
             record.result ? "win" : "defeat",
@@ -35,16 +42,46 @@ function renderRecords() {
 
     winsContainer.textContent = `${wins} Wins`;
     defeatsContainer.textContent = `${defeats} Defeats`;
+    filterResults();
 }
 
 let wins = 0;
 let defeats = 0;
 
+const gameRecords = JSON.parse(localStorage.getItem("gameRecords")) || [];
 const recordsContainer = document.getElementById("records");
 const noRecords = document.getElementById("no-records");
 const winsContainer = document.getElementById("wins");
 const defeatsContainer = document.getElementById("defeats");
 const toggleButton = document.getElementById("theme-toggle");
+const checkboxes = document.querySelectorAll(".checkbox-filter");
+
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", function () {
+        if (this.checked) {
+            checkboxes.forEach((item) => {
+                if (item !== this) {
+                    item.checked = false;
+                }
+            });
+        }
+        filterResults();
+    });
+});
+
+function filterResults() {
+    const filterOption = Array.from(checkboxes).find((cb) => cb.checked)?.id;
+    const recordElements = recordsContainer.querySelectorAll(".record");
+
+    recordElements.forEach((item) => {
+        if (filterOption === "all-filter" || !filterOption) {
+            item.style.display = "flex";
+        } else {
+            const matchesFilter = item.dataset.result === filterOption;
+            item.style.display = matchesFilter ? "flex" : "none";
+        }
+    });
+}
 
 renderRecords();
 ableToggleTheme(toggleButton);
